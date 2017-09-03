c6fdcbd030973be179161898c24cc659008b798b
lydiahallie <lydiajuliettehallie@gmail.com>
2017-09-03 14:40:10 +0200

    Added favicon and logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import './styles.css';
 const Header = () => (
   <div className="header-wrapper z-depth-2">
     <div className="header">
+      <img src="http://icons.iconarchive.com/icons/icons8/windows-8/128/Cultures-Viking-Helmet-icon.png" alt="Viking Logo" id="logo" />
       <h1>Scandinavian Coders</h1>
     </div>
   </div>
